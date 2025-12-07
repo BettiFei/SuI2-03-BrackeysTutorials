9f6e48e1608f51219c4d0f2eda50454fedab3d7d
BettiFei <bettina.feibel@uni-bayreuth.de>
2025-12-07 15:41:26 +0100

    Level Design tweaks

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var speed = 130.0
 @export var jump_velocity = -300.0
@@ -11,12 +12,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip sprite:
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
+	
+	# Play animations:
+	if is_on_floor():
+		if direction == 0:
+			sprite.play("idle")
+		else:
+			sprite.play("run")
+	else:
+		sprite.play("jump")
+	
+	# Apply movement:
 	if direction:
 		velocity.x = direction * speed
 	else:
